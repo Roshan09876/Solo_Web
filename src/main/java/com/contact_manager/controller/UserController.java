67f68d83a81c3ee1ac326f53cf5fca0d65865142
Roshan09876 <khadkaroshan715@gmail.com>
2023-02-02 14:15:05 +0545

    image

--- src/main/java/com/contact_manager/controller/UserController.java
+++ src/main/java/com/contact_manager/controller/UserController.java
@@ -76,7 +76,7 @@ public class UserController {
 //            processing and uploading file
             if(file.isEmpty()){
                 System.out.println("File is Empty");
-                contact.setImage("contact1.png");
+                contact.setImage("contactlogo.png");
             }
             else{
 //                Upload the file to folder and update the name to contact
