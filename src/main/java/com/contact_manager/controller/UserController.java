593a371dbe788a8c896d738b0a77f01a24daac4f
Roshan09876 <khadkaroshan715@gmail.com>
2023-02-04 12:57:52 +0545

    profile Page

--- src/main/java/com/contact_manager/controller/UserController.java
+++ src/main/java/com/contact_manager/controller/UserController.java
@@ -260,4 +260,13 @@ public class UserController {
         return "redirect:/user/"+contact.getcId()+"/contact";
     }
 
+
+//    Profile Handler
+    @GetMapping("/profile")
+    public String yourProfile(Model model){
+
+        model.addAttribute("title", "Profile Page");
+        return "normal/profile";
+    }
+
 }
